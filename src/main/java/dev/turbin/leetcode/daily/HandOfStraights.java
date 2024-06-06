389eec800202f3cd28c5082f7f449f509860121d
Турбин <iturbin@phoenixit.ru>
2024-06-06 18:56:33 +0300

    06/06/2024

---
+++ src/main/java/dev/turbin/leetcode/daily/HandOfStraights.java
@@ -0,0 +1,65 @@
+package dev.turbin.leetcode.daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize,
+ * and consists of groupSize consecutive cards.
+ */
+@SpringBootTest
+public class HandOfStraights {
+
+    @Test
+    void test1() {
+        assertThat(isNStraightHand(new int[]{1,2,3,6,2,3,4,7,8}, 3)).isEqualTo(true);
+    }
+
+    @Test
+    void test2() {
+        assertThat(isNStraightHand(new int[]{1,2,3,4,5}, 4)).isEqualTo(false);
+    }
+
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+
+        if (hand.length % groupSize != 0) {
+            return false;
+        }
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        for (int j : hand) {
+            Integer i1 = map.get(j);
+            map.put(j, i1 == null ? 1 : i1 + 1);
+        }
+
+        Map.Entry<Integer, Integer> entry = null;
+        for (int i = 0; i < hand.length; i++) {
+
+            if (i%groupSize == 0) {
+                System.out.println("group: ");
+                entry = map.firstEntry();
+            } else {
+                entry = map.ceilingEntry(entry.getKey()+1);
+            }
+
+            if (i%groupSize != groupSize -1 && map.get(entry.getKey() + 1) == null) {
+                return false;
+            }
+
+            System.out.println(entry.getKey());
+            if (entry.getValue() == 1) {
+                map.remove(entry.getKey());
+            } else {
+                map.put(entry.getKey(), entry.getValue()-1);
+            }
+        }
+
+        return true;
+    }
+}
--- src/main/java/dev/turbin/leetcode/other/LongestPalindromeByConcat.java
+++ src/main/java/dev/turbin/leetcode/other/LongestPalindromeByConcat.java
@@ -3,10 +3,6 @@ package dev.turbin.leetcode.other;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
