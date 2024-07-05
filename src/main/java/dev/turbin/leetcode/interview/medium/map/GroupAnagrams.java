90d425c5623832caef00f984dd348da3489c022c
Турбин <iturbin@phoenixit.ru>
2024-07-05 19:24:55 +0300

    7/5/2024

--- src/main/java/dev/turbin/leetcode/interview/medium/map/GroupAnagrams.java
+++ src/main/java/dev/turbin/leetcode/interview/medium/map/GroupAnagrams.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
