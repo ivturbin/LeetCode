b4ed4471537023695e97e1ebf21f3f12b9b60f4b
Turbin <iturbin@phoenixit.ru>
2024-07-27 15:18:00 +0300

    07/27/2024

--- src/main/java/dev/turbin/leetcode/interview/medium/intervals/MergeIntervals.java
+++ src/main/java/dev/turbin/leetcode/interview/medium/intervals/MergeIntervals.java
@@ -3,6 +3,8 @@ package dev.turbin.leetcode.interview.medium.intervals;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,31 +24,20 @@ public class MergeIntervals {
     }
 
 
-    //todo: wrong solution
     public int[][] merge(int[][] intervals) {
-        byte[] indexation = new byte[10000];
+        if (intervals.length == 0) return new int[0][0];
 
-        for (int i = 0; i < intervals.length; i++) {
-            for (int j = intervals[i][0]; j < intervals[i][1]; j++) {
-                indexation[j]++;
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        LinkedList<int[]> merged = new LinkedList<>();
+
+        for (int[] interval : intervals) {
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
             }
         }
 
-        int count = 0;
-        int[] interval = null;
-
-        for (int i = 0; i < indexation.length; i++) {
-            if (interval == null && indexation[i] != 0) {
-                interval = new int[2];
-                interval[0] = i;
-            } else if (interval != null && indexation[i] == 0 && indexation[i-1] != 0) {
-                interval[1] = i;
-                intervals[count] = interval;
-                count++;
-                interval = null;
-            }
-        }
-
-        return Arrays.copyOfRange(intervals, 0, count);
+        return merged.toArray(new int[merged.size()][]);
     }
 }
